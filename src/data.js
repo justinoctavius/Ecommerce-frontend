9f8f7f2393a8fa3a134813cc84e32904e0ff7f22
octavio <justinoctavio2001@gmail.com>
2020-07-29 22:26:30 -0400

    first commit 60% finish lack the login register validation etc

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default {
+    products: [
+        {   
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: ''
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 30,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: ''
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: ''
+        }
+]
+}
